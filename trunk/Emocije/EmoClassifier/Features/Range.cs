59f48b55d2eaa70591c583654d2fdae71d48fa41
katarina.cindric2@gmail.com <katarina.cindric2@gmail.com@b48594f6-b757-21b6-8046-b9e75758c3db>
2012-01-12 22:12:03 +0000

    k

---
+++ trunk/Emocije/EmoClassifier/Features/Range.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EmoClassifier.Features
+{
+    class Range : IFeature
+    {
+        #region IFeature Members
+
+        public string Description
+        {
+            get { return "Computes range"; }
+        }
+
+        public double Feature { get; private set; }
+        public IDataProvider DataProvider { get; set; }
+
+        public void Compute()
+        {
+            List<double> Data = DataProvider.Data;
+            Feature = Data.Max() - Data.Min();
+        }
+
+        #endregion
+
+    }
+}
