40c98cb61f5e0b604140f039444f9a04a5040f8a
katarina.cindric2 <katarina.cindric2@gmail.com>
2012-01-12 22:13:11 +0000

    k

---
+++ Emocije/EmoClassifier/Features/Variance.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EmoClassifier.Features
+{
+    public class Variance : IFeature
+    {
+        #region IFeature Members
+
+        public string Description
+        {
+            get { return "Computes variance"; }
+        }
+
+        public double Feature { get; private set; }
+        public IDataProvider DataProvider { get; set; }
+
+        public void Compute() 
+        {   
+           
+            List<double> Data = DataProvider.Data;
+            double average = Data.Average();
+            double sum = 0.0;
+            foreach(double b in Data)
+            {
+              
+                sum += Math.Pow((b-average), 2);
+            }
+            int n = Data.Count;
+            Feature = sum/(n-1);
+        }
+
+        #endregion
+    }
+}
