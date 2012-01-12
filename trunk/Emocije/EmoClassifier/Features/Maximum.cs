315c1b3801550896075b740581147e9a2b725592
katarina.cindric2@gmail.com <katarina.cindric2@gmail.com@b48594f6-b757-21b6-8046-b9e75758c3db>
2012-01-12 22:09:53 +0000

    k

---
+++ trunk/Emocije/EmoClassifier/Features/Maximum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace EmoClassifier.Features
+{
+    public class Maximum : IFeature
+    { 
+
+        #region IFeature Members
+
+        public string Description
+        {
+            get { return "Maximum value"; }
+       }
+
+        public double Feature
+        {
+            get;
+            private set;
+        }
+
+        public IDataProvider DataProvider { get; set; }
+
+        public void Compute()
+        {
+            List<double> Data = DataProvider.Data;
+            Feature = Data.Max();
+        }
+
+        #endregion
+
+    }
+}
